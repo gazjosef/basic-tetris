16b31058e2fba4a272589d26c0d489d5e856cf23
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-05-24 17:55:02 +1000

    Drawing Tetrominos

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 // EVent Listeners
-document.addEventListener(('click') => {
-  // Somthing goes here
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const ScoreDisplay = document.querySelector('#score');
+  const StartBtn = document.querySelector('#start-button');
+  const width = 10;
+
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+  ];
+
+  console.log(squares);
 });
