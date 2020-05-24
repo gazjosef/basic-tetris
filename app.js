2e18bebeadb03a770b7e43b0b88af46dc1936c74
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-05-24 22:22:46 +1000

    Times and intervals

--- app.js
+++ app.js
@@ -82,5 +82,24 @@ document.addEventListener('DOMContentLoaded', () => {
     undraw();
     currentPosition += width;
     draw();
+    freeze();
+  }
+
+  // Freeze Function
+  function freeze() {
+    if (
+      current.some((index) =>
+        squares[currentPosition + index + width].classList.contains('taken')
+      )
+    ) {
+      current.forEach((index) =>
+        squares[currentPosition + index].classList.add('taken')
+      );
+      // Start a new tetromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
   }
 });
