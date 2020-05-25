3e3cd8e10c0601f0575ca575b7d4f65c53d32b30
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-05-25 22:11:36 +1000

    Assigning colors to our Tetrominos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// EVent Listeners
+// Event Listeners
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   let squares = Array.from(document.querySelectorAll('.grid div'));
