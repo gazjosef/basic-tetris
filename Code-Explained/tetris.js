b17d0a6a36d5a9e7b7e9038ef64e2dc8246a3d21
gazjosef <39395712+gazjosef@users.noreply.github.com>
2020-06-18 22:18:30 +1000

    Updated Code-Explained Tetris

--- Code-Explained/tetris.js
+++ Code-Explained/tetris.js
@@ -3,12 +3,47 @@ const ctx = cvs.getContext('2d');
 
 const ROW = 20;
 const COL = (COLUMN = 10);
-const SQ = (squareSize = 10);
+const SQ = (squareSize = 20);
 const VACANT = 'WHITE'; // Color of an empty square
 
 // Draw A Square
-ctx.fillStyle = 'red';
-ctx.fillRect(0, 0, 50, 50);
+function drawSquare(x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x * SQ, y * SQ, SQ, SQ);
 
-ctx.strokeStyle = 'BLACK';
-ctx.strokeRect(0, 0, 50, 50);
+  ctx.strokeStyle = 'BLACK';
+  ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
+}
+
+// Create The Board
+let board = [];
+for (r = 0; r < ROW; r++) {
+  board[r] = [];
+  for (c = 0; c < COL; c++) {
+    board[r][c] = VACANT;
+  }
+}
+
+// Draw The Board Onto Canvas
+function drawBoard() {
+  for (r = 0; r < ROW; r++) {
+    for (c = 0; c < COL; c++) {
+      drawSquare(c, r, board[r][c]);
+    }
+  }
+}
+
+drawBoard();
+
+// The Object Piece
+function Piece(tetromino, color) {
+  this.tetromino = tetromino;
+  this.color = color;
+
+  this.tetrominoN = 0;
+  this.activeTetromino = tetromino[tetrominoN];
+
+  // Control Pieces
+  this.x = 0;
+  this.y = 0;
+}
